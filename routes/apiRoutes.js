0cc08626fa63b807db70e79b03f09cbd9dc9a5ae
Alexandria Robertson <alexemrob@gmail.com>
2020-09-24 09:43:04 -0500

    complete apiRoutes.js delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -47,9 +47,26 @@ module.exports = function (app) {
 
     });
 
-    app.post("/api/notes/:id", function (req, res) {
-//use id to locate in array and remve
-//rewrite to db.json
+
+    //use id to locate in array and remve
+    //rewrite to db.json
+    app.delete("/api/notes/:id", function (req, res) {
+        fs.readFile("db/db.json", "utf8", function (error, data) {
+            let noteId = req.params.id;
+            let noteData = JSON.parse(data);
+            noteData = noteData.filter(function (note) {
+                if (noteId != note.id) {
+                    return true;
+                } else {
+                    return false;
+                };
+            });
+            fs.writeFile("db/db.json", JSON.stringify(noteData), function (error) {
+                if (error)
+                    throw error;
+                res.end(console.log("Deleted Successfully"));
+            })
+        });
 
     });
 
